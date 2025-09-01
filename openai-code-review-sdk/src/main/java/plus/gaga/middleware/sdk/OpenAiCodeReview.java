63ff6787c25bf3af59474e0ce1359f0bf798c4fc
yingying2223 <yingying2223@noreply.gitcode.com>
2025-09-01 10:51:13 +0800

    微信通知

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -5,8 +5,10 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import plus.gaga.middleware.sdk.domain.model.ChatCompletionRequestDTO;
 import plus.gaga.middleware.sdk.domain.model.ChatCompletionSyncResponseDTO;
+import plus.gaga.middleware.sdk.domain.model.Message;
 import plus.gaga.middleware.sdk.domain.model.Model;
 import plus.gaga.middleware.sdk.types.utils.BearerTokenUtils;
+import plus.gaga.middleware.sdk.types.utils.WXAccessTokenUtils;
 
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -16,6 +18,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
+import java.util.Scanner;
 
 public class OpenAiCodeReview {
 
@@ -53,6 +56,8 @@ public class OpenAiCodeReview {
         // 3. 写入评审日志
         String logUrl = writeLog(token, log);
         System.out.println("writeLog：" + logUrl);
+
+        pushMessage(logUrl);
     }
 
     private static String codeReview(String diffCode) throws IOException {
@@ -110,6 +115,43 @@ public class OpenAiCodeReview {
 
 
     }
+
+    private static void pushMessage(String logUrl) throws IOException {
+        String accessToken = WXAccessTokenUtils.getAccessToken();
+        System.out.println(accessToken);
+
+        Message message = new Message();
+        message.put("project","big-market");
+        message.put("review","logUrl");
+        message.setUrl(logUrl);
+        String url = String.format("https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=%s", accessToken);
+        sendPostRequest(url, JSON.toJSONString(message));
+    }
+
+
+    private static void sendPostRequest(String urlString, String jsonBody) {
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; utf-8");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+
+            try (OutputStream os = conn.getOutputStream()) {
+                byte[] input = jsonBody.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            try (Scanner scanner = new Scanner(conn.getInputStream(), StandardCharsets.UTF_8.name())) {
+                String response = scanner.useDelimiter("\\A").next();
+                System.out.println(response);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private static String writeLog(String token, String log) throws Exception {
         Git git = Git.cloneRepository()
                 .setURI("https://github.com/metryr/openai-code-review2-log.git")
---
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/domain/model/Message.java
@@ -0,0 +1,55 @@
+package plus.gaga.middleware.sdk.domain.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Message {
+
+    private String touser = "o0Xf21wvolAz9HXWkRK1_sVxE6as";
+    private String template_id = "GWOxa5a1TUk_v7-_TV2wNGISlUal5SWmHIQ1I4zJ0eU";
+    private String url = "https://weixin.qq.com";
+    private Map<String, Map<String, String>> data = new HashMap<>();
+
+    public void put(String key, String value) {
+        data.put(key, new HashMap<String, String>() {
+            private static final long serialVersionUID = 7092338402387318563L;
+
+            {
+                put("value", value);
+            }
+        });
+    }
+
+    public String getTouser() {
+        return touser;
+    }
+
+    public void setTouser(String touser) {
+        this.touser = touser;
+    }
+
+    public String getTemplate_id() {
+        return template_id;
+    }
+
+    public void setTemplate_id(String template_id) {
+        this.template_id = template_id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Map<String, Map<String, String>> getData() {
+        return data;
+    }
+
+    public void setData(Map<String, Map<String, String>> data) {
+        this.data = data;
+    }
+
+}
